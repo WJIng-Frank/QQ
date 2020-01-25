c4c718eafbf4d5942a1c78268b961c1c6b38c9b5
WJIng-Frank <1341518335@qq.com>
2020-01-25 15:00:23 +0800

    [ADD]添加manager文件

---
+++ manager.py
@@ -0,0 +1,12 @@
+# !/usr/bin/python3
+
+# -*- coding: utf-8 -*-
+
+# @Author   : Wu JIng
+
+# @FILE     : manager.py
+
+# @Time     : 2020/1/25 14:59
+
+# @Software : PyCharm
+"""this is manager.py"""
